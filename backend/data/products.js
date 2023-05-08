d9dceab7ca9454ada526beaef46b12367979aa0e
Amit Kumar Ashutosh <73929517+amitkumarashutosh@users.noreply.github.com>
2023-05-09 03:06:36 +0530

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,53 @@
+const products = [
+  {
+    name: 'Men Tan Brown Waterproof Loafers',
+    image: '/images/airpods.png',
+    description:
+      'A pair of round toe tan loafers ,has mid-top styling, Slip-on detail, Synthetic upper, Cushioned footbed,Textured and patterned outsole',
+    brand: 'Aqualite',
+    category: 'Shoes',
+    price: 3.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Men Red & Black Pure Cotton Shirt',
+    image: '/images/shirt.png',
+    description:
+      'Red and black checked casual shirt, has a spread collar, button placket, 1 pocket, long sleeves, curved hem',
+    brand: 'Slack',
+    category: 'Shirt',
+    price: 7.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Men Slim Tapered Fit Low-Rise Heavy Fade Jeans',
+    image: '/images/jeans.png',
+    description:
+      'Medium shade, heavy fade blue jeans, Slim fit, low-rise, Clean look, Stretchable, 5 pocket',
+    brand: 'Junix',
+    category: 'Jeans',
+    price: 9.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Women’s Wear Red Color Taffeta Silk Lehenga',
+    image: '/images/suit.png',
+    description:
+      'Chain Stitch Embroidery With Mirror And Stone Work, Half Sleeves With Chain Stitch Embroidery With Stone Work',
+    brand: 'LNT',
+    category: 'Lehenga',
+    price: 27.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  
+]
+
+export default products
